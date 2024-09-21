2c54dc10956e3610c550759861f209ccd3e747ad
Alfisha-Anjum <anjumalfisha@gmail.com>
2024-09-21 16:11:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Main from "./Pages/Main";
+import Blog from "./Pages/Blog";
+import Transaction from "./Pages/Transaction";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Main />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/transaction" element={<Transaction />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
